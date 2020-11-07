9d70b9d1098f4d3d6c52d214430c818a1fb188f6
AGunnarP <53379060+AGunnarP@users.noreply.github.com>
2020-11-07 12:45:37 -0800

    Update main.js

--- Brazil IMO/main.js
+++ Brazil IMO/main.js
@@ -3,5 +3,7 @@
 const Discord = require('discord.js');
 
 //This is the client that the discord bot runs on. The client login is the token needed for the bot to go online
+
 const client = new Discord.Client();
-client.login('Nzc0NzMwMjMxNTc5NTQxNTM2.X6cBwQ.kCTrdOYCx71tsbiStluOS4ZCEuE');
+export const token = process.env['token'];
+client.login(token);
