8778eef145b7399ca5bf0289ce34a225c6e6eb85
AGunnarP <53379060+AGunnarP@users.noreply.github.com>
2020-11-07 12:38:57 -0800

    Update main.js

--- Brazil IMO/main.js
+++ Brazil IMO/main.js
@@ -1,1 +1,7 @@
 // JavaScript source code
+
+const Discord = require('discord.js');
+
+//This is the client that the discord bot runs on. The client login is the token needed for the bot to go online
+const client = new Discord.Client();
+client.login('Nzc0NzMwMjMxNTc5NTQxNTM2.X6cBwQ.kCTrdOYCx71tsbiStluOS4ZCEuE');
