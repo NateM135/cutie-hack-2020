8ef3c28a1d86be5630ac53a15891cb3501531794
Nathan Melwani <melwaninate@gmail.com>
2020-11-07 17:04:56 -0800

    Added command to clear messages

---
+++ Brazil IMO/commands/clear.js
@@ -0,0 +1,37 @@
+module.exports = {
+    name: "clear",
+    aliases: ["purge", "nuke"],
+    description: "Clears the chat",
+    execute(message, args) {
+        if (message.deletable) {
+            message.delete();
+        }
+    
+        // Member doesn't have permissions
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("You can't delete messages....").then(m => m.delete(5000));
+        }
+
+        // Check if args[0] is a number
+        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+            return message.reply("You must specify an int (integer) number of messages to delete. The number must be positive and not 0.").then(m => m.delete(5000));
+        }
+
+        // Maybe the bot can't delete messages
+        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("I don't have the permissions needed to delete messages.").then(m => m.delete(5000));
+        }
+
+        let deleteAmount;
+
+        if (parseInt(args[0]) > 100) {
+            deleteAmount = 100;
+        } else {
+            deleteAmount = parseInt(args[0]);
+        }
+
+        message.channel.bulkDelete(deleteAmount, true)
+            .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages.`))
+            .catch(err => message.reply(`Something went wrong... ${err}`));
+    }
+}
