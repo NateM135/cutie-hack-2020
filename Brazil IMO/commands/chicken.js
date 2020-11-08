aa95ee62e5af8c22ab0986bede69b5cacb4d57ea
angelikab028 <74116754+angelikab028@users.noreply.github.com>
2020-11-07 17:25:52 -0800

    Added chicken command

---
+++ Brazil IMO/commands/chicken.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'chicken',
+    description: 'pollo',
+    execute(message) {
+        message.channel.send("", {files: ["https://i.imgur.com/RrjoNs9.jpg"]});
+    
+    },
+};
