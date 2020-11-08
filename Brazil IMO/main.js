46484d95cabc06c2a956ce948d6ffbe8454d9b59
AGunnarP <53379060+AGunnarP@users.noreply.github.com>
2020-11-07 16:52:16 -0800

    Update main.js

--- Brazil IMO/main.js
+++ Brazil IMO/main.js
@@ -7,6 +7,8 @@ client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+var regexes = ['(n( )*(i)*( )*(g+( )*g*|g+)( )*e+( )*r)', '(n( )*(i)*( )*(g+( )*g*|g+)( )*a*( )*r*)', '(n( )*!*1*(i)*( )*(g+( )*g*|g+)( )*4*a*( )*r*)', '(n( )*!*1*(i)*( )*(g+( )*g*|g+)( )*4*a*( )*r*)', 'n(a)*te','f( )*(a)*( )*g*( )*g+( )*(o)*( )*t'];
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
@@ -17,6 +19,39 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
+	
+	console.log('message.content=' + message.content);
+
+	if (message.author.bot) return;
+
+	for (let regex of regexes) {
+
+		let refinedmessage = message.content.replace('\.*,*\'*(\*)*\(*\)*\{*\}*\\*\/*\[*\]*', '');
+
+		let regularexpression = new RegExp(regex);
+
+		let joke = new RegExp('l(o*)ng l(o*)ng m(a*)n');
+
+		if (regularexpression.test(refinedmessage || message.author.id == '299685173262286849') == true) {
+
+			console.log(regex);
+
+			message.reply('@' + message.author + 'https://i.kym-cdn.com/photos/images/newsfeed/001/869/628/370.gif');
+
+			message.delete();
+
+			break;
+
+		}
+		else if (joke.test(refinedmessage)){
+
+			message.reply('https://www.youtube.com/watch?v=6-1Ue0FFrHY');
+			break;
+
+        }
+
+    }
+	
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
